a582fd904d66486c0126aa69a08fad9ec6083328
Patrick Liepins <patty.liepins@googlemail.com>
2016-12-23 22:16:24 +0100

    initial commit

---
+++ src/main/java/de/start/knower/AromenMerkerApplication.java
@@ -0,0 +1,12 @@
+package de.start.knower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AromenMerkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AromenMerkerApplication.class, args);
+	}
+}
