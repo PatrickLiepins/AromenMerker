b897b044fbdb73ce5fcf9b8c9c0d454918026201
Patrick Liepins <patty.liepins@googlemail.com>
2016-12-23 22:57:16 +0100

    create model flavor and producer

---
+++ src/main/java/de/start/knower/model/Flavor.java
@@ -0,0 +1,65 @@
+package de.start.knower.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity @Getter @Setter @ToString
+public class Flavor implements Serializable {
+    
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    @OneToOne()
+    private Producer producer;
+    private int mixture;
+    private double price;
+
+    public Flavor(String name, Producer producer, int mixture, double price) {
+        this.name = name;
+        this.producer = producer;
+        this.mixture = mixture;
+        this.price = price;
+    }
+
+    
+
+    public Flavor() {
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Flavor other = (Flavor) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+}
---
+++ src/main/java/de/start/knower/model/Producer.java
@@ -0,0 +1,55 @@
+package de.start.knower.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity @Getter @Setter @ToString 
+public class Producer implements Serializable {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    public Producer(String name) {
+        this.name = name;
+    }
+
+    public Producer() {
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Producer other = (Producer) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
+    
+
+}
