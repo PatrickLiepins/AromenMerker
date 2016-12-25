c267ba2d00f03a1dbe080b3f7b49d13f6203b36f
Patrick Liepins <patty.liepins@googlemail.com>
2016-12-25 23:26:08 +0100

    insert the delete mapping in the recipeController

--- src/main/java/de/start/knower/controller/RecipeController.java
+++ src/main/java/de/start/knower/controller/RecipeController.java
@@ -1,11 +1,13 @@
 package de.start.knower.controller;
 
+import de.start.knower.model.Recipe;
 import de.start.knower.repository.RecipeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/recipes")
@@ -19,10 +21,17 @@ public class RecipeController {
 
     @GetMapping("{recipeId}")
     public String viewRecipe(final @PathVariable Long recipeId, final Model model) {
-        
+
         model.addAttribute("recipe", recipeRepository.findOne(recipeId));
 
         return "recipe/index";
+    }
+
+    @GetMapping("{recipeId}/delete")
+    public String deleteRecipe(final @PathVariable Long recipeId,
+            @RequestParam Long userId) {
+        recipeRepository.delete(recipeId);
+        return "redirect:/users-dashboard/" + userId;
     }
 
 }
